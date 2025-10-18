2bb6b51957f0322e89c35f1f9f4a249c18db22db
kassu11 <61124141+kassu11@users.noreply.github.com>
2025-10-18 13:01:05 +0300

    rework: 🛠️ Change complex Regex to state machine
    - This should be the final version if no bugs are found
    - Added pseudo selector support and basically everything I could think
      of

--- local-style.js
+++ local-style.js
@@ -1,15 +1,3 @@
-export function splitmix32(a) {
-  return function() {
-    a |= 0;
-    a = (a + 0x9e3779b9) | 0;
-    let t = a ^ (a >>> 16);
-    t = Math.imul(t, 0x21f0aaad);
-    t = t ^ (t >>> 15);
-    t = Math.imul(t, 0x735a2d97);
-    return ((t = t ^ (t >>> 15)) >>> 0) / 4294967296;
-  };
-}
-
 export default function solidSfcPlugin() {
   return {
     name: "vite-plugin-solid-sfc",
@@ -22,32 +10,118 @@ export default function solidSfcPlugin() {
         const attribute = "[" + localDataAttributeFromFilePath(id) + "]";
 
         // Get list of all queries inside the file
-        return src.replace(/(}|;|^)((\\n|\\r|\s)*)([^;}]+?)((\\n|\\r|\s)*)(?={)/g, (match, start = "", leftPad1 = "", _a, queries, rightPad1 = "", _b) => {
-          // Query is media query skip current query
+        return src.replace(/(}|;|^|{)(\s*)([^;}]+?)(\s*)(?={)/g, (match, start = "", leftPad1 = "", queries, rightPad1 = "") => {
+          // Query is media or container query skip current query
           if (queries.startsWith("@")) {
             return match;
           }
 
           // Loop through queries
           const formatedQueries = queries.split(",").map(query => {
-            return query.replace(/((\s|\\n|\\r|^)+)(.+?)((\s|\\n|\\r|$)+)/g, (match, leftPad1 = "", _a, selector, rightPad1 = "", _b) => {
-              // We can skip & because the parent should already have scoped styling
-              // Also skip :hover and pseudo elements
-              // TODO :has(.class) is not handled here
-              if (selector.startsWith("&") || selector.startsWith(":")) {
-                return match;
-              }
+            let returnQuery = "";
 
-              // Use "_" to make styling global
-              if (selector.startsWith("_") || selector.startsWith("#_") || selector.startsWith("._")) {
-                return leftPad1 + selector.replace("_", "") + rightPad1;
-              }
+            let scopeCurrentSelector = true;
+            let hasSelector = false;
+            let isPseudoClass = false;
+            const pseudoSelectorScope = [];
+            const attributeScope = [];
+            const attributeScopeCharacters = {
+              "\"": "\"",
+              "'": "'",
+              "[": "]",
+            };
 
-              const selectors = selector.split(":");
-              selectors[0] += attribute;
-              // TODO :has(.class) is not handled here
-              return leftPad1 + selectors.join(":") + rightPad1;
-            });
+            for (let i = 0; i < query.length; i++) {
+              // Position starts with either attribute or string
+              if (queries[i - 1] !== "\\" && queries[i] in attributeScopeCharacters) {
+                attributeScope.push(queries[i]);
+                returnQuery += query[i];
+              }
+              // Position ends with either attribute or string
+              else if (attributeScope.length && queries[i - 1] !== "\\" && queries[i] === attributeScopeCharacters[attributeScope.at(-1)]) {
+                attributeScope.pop();
+                returnQuery += query[i];
+              }
+              // Prevent selector splitting inside attribute or string inside attribute
+              else if (attributeScope.length) {
+                returnQuery += query[i];
+              }
+              // Position char ends the current selector
+              // Check if we want to scope the selector
+              else if (query[i] === " " || query[i] === ")" || query[i] === "+" || query[i] === "~" || query[i] === ">" || query[i] === "\n" || query[i] === "\r") {
+                if (scopeCurrentSelector && hasSelector) {
+                  returnQuery += attribute;
+                }
+                // If selector was already scoped before the pseudo selector no need to rescope the selector
+                // This check will just make CSS file little smaller
+                if (query[i] === ")") {
+                  const selectorAlreadyScoped = pseudoSelectorScope.pop();
+                  scopeCurrentSelector = selectorAlreadyScoped;
+                } else {
+                  scopeCurrentSelector = true;
+                }
+                hasSelector = false;
+                isPseudoClass = false;
+                returnQuery += query[i];
+              }
+              // No need to scope the current selector because the parent should handle the scoping
+              else if (query[i] === "&") {
+                scopeCurrentSelector = false
+                isPseudoClass = false;
+                returnQuery += query[i];
+              }
+              // Selector contains pseudo selector with arguments
+              // Scope the pseudo selectors insides if they need it
+              else if (isPseudoClass && query[i] === "(") {
+                pseudoSelectorScope.push(scopeCurrentSelector);
+                // These pseudo classes don't take classes as input so don't scope them
+                // We don't want :nth-child(3) to become: :nth-child(3[data-k-234234])
+                // Pseudo class that we want to scope could be :has, :where: :is, :not etc.
+                if (
+                  equalsBackwards("child(", query, i) ||
+                  equalsBackwards("type(", query, i) ||
+                  equalsBackwards(":dir(", query, i) ||
+                  equalsBackwards(":state(", query, i) ||
+                  equalsBackwards(":host(", query, i) ||
+                  equalsBackwards(":context(", query, i)
+                ) {
+                  scopeCurrentSelector = false;
+                } else {
+                  scopeCurrentSelector = true
+                }
+                hasSelector = false;
+                returnQuery += query[i];
+              }
+              // Position start normal pseudo selector or pseudo selector with arguments
+              // args: :not(.class) no args: :hover
+              else if (query[i] === ":") {
+                // Don't scope :root, if there are any other pseudo selectors that you should not scope add them here
+                if (equalsforwards(queries, ":root", i)) {
+                  scopeCurrentSelector = false;
+                }
+                else if (scopeCurrentSelector) {
+                  returnQuery += attribute;
+                }
+                scopeCurrentSelector = false;
+                isPseudoClass = true;
+                returnQuery += query[i];
+              }
+              // Selector is prefixed with "_" don't scope the selector
+              else if (hasSelector === false && queries[i] === "_") {
+                scopeCurrentSelector = false;
+              }
+              // Position should have ".", "#", and rest of UTF-8 characters
+              else {
+                hasSelector = true;
+                returnQuery += query[i];
+              }
+            }
+
+            if (scopeCurrentSelector && hasSelector) {
+              returnQuery += attribute;
+            }
+
+            return returnQuery;
           }).join(",");
 
           return start + leftPad1 + formatedQueries + rightPad1;
@@ -55,6 +129,32 @@ export default function solidSfcPlugin() {
       }
     },
   };
+}
+
+const splitmix32 = (a) => {
+  return () => {
+    a |= 0;
+    a = (a + 0x9e3779b9) | 0;
+    let t = a ^ (a >>> 16);
+    t = Math.imul(t, 0x21f0aaad);
+    t = t ^ (t >>> 15);
+    t = Math.imul(t, 0x735a2d97);
+    return ((t = t ^ (t >>> 15)) >>> 0) / 4294967296;
+  };
+}
+
+const equalsBackwards = (string, string2, index) => {
+  return equalsforwards(string, string2, index - (string.length - 1));
+}
+
+const equalsforwards = (string, string2, index) => {
+  for (let i = 0; i < string.length; i++) {
+    if (string2[i + index] !== string[i]) {
+      return false;
+    }
+  }
+
+  return true;
 }
 
 const stringBetweenTwoChars = (a, b) => {
